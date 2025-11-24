14575254a53f999bbae2aaf48e6198a6037e95cb
heyeepc <2675458141@qq.com>
2025-11-24 10:52:57 +0800

    Create main.py

---
+++ 04.第四章/01.计算斜边（模块化处理）/main.py
@@ -0,0 +1,11 @@
+def main():
+    while True:
+        show_menu()
+        choice = input(...)
+
+        if choice == '1':
+            single_mode()
+        elif choice == '2':
+            loop_mode()
+        elif choice == 'q':
+            break

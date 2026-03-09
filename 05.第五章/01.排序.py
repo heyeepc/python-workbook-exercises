487a9718c39e17eb45e608211d8764fd911f55e7
heyeepc <2675458141@qq.com>
2026-03-09 13:13:40 +0800

    Update 01.排序.py

--- 05.第五章/01.排序.py
+++ 05.第五章/01.排序.py
@@ -1,1 +1,26 @@
+# 编写一个程序，从用户处读取整数，并将它们存储在一个列表中。程序应该继续读取值，直到用户输入0。
+# 然后，它应该按升序显示用户输入的所有值（除了0），每行显示一个值。使用 sort方法或 sorted（）函数对列表进行排序。
 
+numbers = []
+
+print("请输入整数（输入 0 结束）：")
+
+while True:
+    try:
+        # 读取用户输入并转换为整数
+        user_input = int(input("> "))
+
+        # 如果输入是 0，则跳出循环
+        if user_input == 0:
+            break
+
+        # 将输入的数字添加到列表中
+        numbers.append(user_input)
+    except ValueError:
+        print("请输入有效的整数！")
+
+
+sorted_numbers = sorted(numbers)
+
+for num in sorted_numbers:
+    print(num)

f7594e01139146cc0a64e43afb01f33c5ffcc9fd
heyeepc <2675458141@qq.com>
2026-05-31 11:56:49 +0800

    Create 13.Pig Latin.py

---
+++ 05.第五章/13.Pig Latin.py
@@ -0,0 +1,3 @@
+# Pig Latin是通过转化英语单词而构成的一种语言。虽然这种语言的来源是未知的，但它至少在19世纪的两份文件中被提及，表明它已经存在了100多年。将英语翻译成Pig Latin的规则如下：
+# 如果单词以辅音字母开头（包括y），那么单词开头的所有字母，直到第一个元音字母（不包括y），都将被删除，然后添加到单词末尾，后面跟着ay。例如，computer 变成omputercay，think 变成inkthay。
+# 如果单词以元音开头（不包括y），那么把 way加到单词的末尾。例如，algorithm变成algorithmway，office变成officeway。
